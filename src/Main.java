265f64672614d750d7419486ebf8c8aa025b7b9d
tylerwright <wrigh3tr@mail.uc.edu>
2024-09-26 14:51:58 -0400

    Commit and Push

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.println("");
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        double itemPrice = 0.00;
+        final double Ship_Rate = .02;
+        final double FREE_Ship = 100.00;
+        double shipCost = 0.00;
+        double totalCost = 0;
+        String trash = "";
+        System.out.println("Enter the item Price: ");
+        if (console.hasNextInt()) {
+            itemPrice = console.nextDouble();
+            console.nextLine();
+            if (itemPrice >= FREE_Ship) {
+                shipCost = 0;
+
+            } else {
+                shipCost = Ship_Rate * itemPrice;
+                totalCost = itemPrice + shipCost;
+            }
+        } else {
+            trash = console.nextLine();
+            System.out.println("Enter a Valid Input " + trash + " is not a Number");
+            System.out.println("Run the Program again ");
+        }
     }
 }
+
